dcf3fb7fe1ffc2a11d06b286a1f677055708ce9b
Bogdan Kirilenko <kirilenkobm@gmail.com>
2023-09-18 20:09:34 +0200

    little cleanup

--- src/krlnk_image_fft_glitch.c
+++ src/krlnk_image_fft_glitch.c
@@ -44,13 +44,9 @@ void process_chunk(double *image,
 
     // HERE is where the magic could happen
     effect(out, chunk_size);
-//    for (int i = 20; i < chunk_size - 5; ++i) {
-//        out[i][0] = 0;
-//        out[i][1] = 0;
-//    }
 
     // Execute Inverse FFT
-    fftw_plan p_inv = fftw_plan_dft_1d(chunk_size, out, in, FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftw_plan p_inv = fftw_plan_dft_1d(chunk_size, out, in, FFTW_BACKWARD, FFTW_MEASURE);
     fftw_execute(p_inv);
 
     // Copy back to original image array
@@ -72,10 +68,13 @@ void image_fft_glitch(double *image, int size, int chunk_size)
     // Initialize FFTW
     in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * chunk_size);
     out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * chunk_size);
-    p = fftw_plan_dft_1d(chunk_size, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    p = fftw_plan_dft_1d(chunk_size, in, out, FFTW_FORWARD, FFTW_MEASURE);
 
     int offset;
     EffectFunc current_effect = zero_mid_freq;  // Choose your effect here
+
+    // not sure whether it helped me a lot =)
+    #pragma omp parallel for
     for (offset = 0; offset + chunk_size <= size; offset += chunk_size) {
         process_chunk(image, offset, chunk_size, p, in, out, current_effect);
     }
@@ -83,7 +82,7 @@ void image_fft_glitch(double *image, int size, int chunk_size)
     // Handle the last chunk if it's smaller than chunk_size
     int remaining = size - offset;
     if (remaining > 0) {
-        fftw_plan p_last = fftw_plan_dft_1d(remaining, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+        fftw_plan p_last = fftw_plan_dft_1d(remaining, in, out, FFTW_FORWARD, FFTW_MEASURE);
         process_chunk(image, offset, remaining, p_last, in, out, current_effect);
         fftw_destroy_plan(p_last);
     }
